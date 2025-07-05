30a156206ee313d39a6ca1610c2de97d765426f2
charliecollier <collier.p.charlie@gmail.com>
2025-07-05 16:17:49 +0100

    init files like coverage github workflows and scripts github issue and pr templates initial check parquet python script and tests pyproject.toml updates

---
+++ .github/scripts/check-changelog-has-version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VERSION=$1
+CHECK_CHANGELOG=$(grep "\[$VERSION\]" CHANGELOG.md)
+
+if [[ -z $CHECK_CHANGELOG ]]; then
+  echo "No CHANGELOG entry for latest version $VERSION"
+  exit 1
+fi
