05318c665b4dfb203c641ed7f5f553d55e52ded8
charliecollier <collier.p.charlie@gmail.com>
2025-07-05 15:49:30 +0100

    GitHub: - issue templates / PR template - workflows / scripts for validation / publish
    .coveragerc
    initial check for PARQUET type
    unit testing

---
+++ .github/scripts/check-changed-file.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BRANCH_NAME=$1  # {{ github.base_ref }}
+shift
+FILES_TO_CHECK="$@"
+
+FILES_NOT_CHANGED=()
+FILES_CHANGED=$(git diff --name-only origin/$BRANCH_NAME...HEAD)
+
+for file in $FILES_TO_CHECK; do
+  IS_PRESENT=$(echo $FILES_CHANGED | grep "^$file$")  # either empty or the file
+  if [[ -z $IS_PRESENT ]]; then  # if empty no match
+    FILES_NOT_CHANGED+=($file)
+  fi
+done
+
+if [[ ${#FILES_NOT_CHANGED[@]} > 0 ]];
+  echo "The following files which require updates were not updated: $FILES_NOT_CHANGED"
+  exit 1
+fi
+
+echo "All files which require updates were updated"
